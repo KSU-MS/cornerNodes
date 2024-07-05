d54181e61be157cc055b306f81c787583686ad93
ChanceCtrl <StaticElite05@gmail.com>
2024-07-05 19:20:52 -0400

    Added shockpots

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ Metro uBrake = 20;
 
 Analog f_brake;
 Analog r_brake;
+Analog FL_shock;
+Analog FR_shock;
 
 void setup() {
   // Start serial
@@ -17,6 +19,8 @@ void setup() {
   // Start sensors
   f_brake.init(0, true);
   r_brake.init(1, true);
+  FL_shock.init(3, true);
+  FR_shock.init(2, true);
 
   // Setup whatever CAN coms
   init_CAN();
@@ -26,9 +30,13 @@ void loop() {
   // Update our fellows
   f_brake.update();
   r_brake.update();
+  FL_shock.update();
+  FR_shock.update();
 
   if (uBrake.check()) {
     send_CAN(BRAKEPRESSURE_F, sizeof(f_brake.value), f_brake.value.b);
     send_CAN(BRAKEPRESSURE_R, sizeof(r_brake.value), r_brake.value.b);
+    send_CAN(SHOCK_FL, sizeof(FL_shock.value), FL_shock.value.b);
+    send_CAN(SHOCK_FR, sizeof(FR_shock.value), FR_shock.value.b);
   }
 }
