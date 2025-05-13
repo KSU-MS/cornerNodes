3568ebb24fadba2a079f1c9709777a244d60c644
ChanceCtrl <>
2025-05-12 22:15:26 -0400

    I lieded this is whats running

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ void loop() {
   steering_pot.update();
 
   if (uSteer.check()) {
+    encode_can_0x451_omni_steering(&kms_dbc, steering_pot.value.in);
     steer_pot_msg.length = pack_message(&kms_dbc, CAN_ID_OMNI_STEERING_DATA,
                                         &steer_pot_msg.buf.val);
     can.send_controller_message(steer_pot_msg);
