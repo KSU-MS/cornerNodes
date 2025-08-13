399c46c1def85406ec1cd3eef7ee20665fd8cb9f
ChanceCtrl <>
2025-08-13 02:20:01 -0400

    njihsfdaffsjiD-

--- include/main.hpp
+++ include/main.hpp
@@ -9,11 +9,8 @@ Metro hz_50(20, true);
 
 // Pot things
 #include <adc.hpp>
-adc steering_pot(avr, 21);
 adc left_shock_pot(avr, 20);
 adc right_shock_pot(avr, 15);
-adc rear_brake(avr, 14);
-adc front_brake(avr, 16);
 
 // Can stuffs
 #include "can_tools.hpp"
@@ -22,8 +19,5 @@ can_obj_car_h_t kms_dbc;
 canMan inv_can(TEENSY_CAN1, 500000);
 canMan daq_can(TEENSY_CAN2, 1000000);
 
-can_message left_shock_msg = {.id = CAN_ID_CORNERNODE_FL_SHOCKPOT};
-can_message right_shock_msg = {.id = CAN_ID_CORNERNODE_FR_SHOCKPOT};
-can_message front_brake_msg = {.id = CAN_ID_CORNERNODE_FRONT_BRAKEPRESSURE};
-can_message rear_brake_msg = {.id = CAN_ID_CORNERNODE_REAR_BRAKEPRESSURE};
-can_message steering_pot_msg = {.id = CAN_ID_CORNERNODE_STEERINGPOT};
+can_message left_shock_msg = {.id = CAN_ID_CORNERNODE_RL_SHOCKPOT};
+can_message right_shock_msg = {.id = CAN_ID_CORNERNODE_RR_SHOCKPOT};
--- src/main.cpp
+++ src/main.cpp
@@ -5,61 +5,30 @@ void setup() {
   Serial.begin(9600);
 
   // Init the pins
-  pinMode(21, INPUT);
   pinMode(20, INPUT);
-  pinMode(16, INPUT);
   pinMode(15, INPUT);
-  pinMode(14, INPUT);
 }
 
 void loop() {
   // Update our fellows
   left_shock_pot.update();
   right_shock_pot.update();
-  front_brake.update();
-  rear_brake.update();
-  steering_pot.update();
 
   if (hz_50.check()) {
-    encode_can_0x384_cornernode_fl_shockpot(&kms_dbc, left_shock_pot.value.in);
+    encode_can_0x386_cornernode_rl_shockpot(&kms_dbc, left_shock_pot.value.in);
     left_shock_msg.length = pack_message(
-        &kms_dbc, CAN_ID_CORNERNODE_FL_SHOCKPOT, &left_shock_msg.buf.val);
+        &kms_dbc, CAN_ID_CORNERNODE_RL_SHOCKPOT, &left_shock_msg.buf.val);
 
-    encode_can_0x385_cornernode_fr_shockpot(&kms_dbc, right_shock_pot.value.in);
+    encode_can_0x387_cornernode_rr_shockpot(&kms_dbc, right_shock_pot.value.in);
     right_shock_msg.length = pack_message(
-        &kms_dbc, CAN_ID_CORNERNODE_FR_SHOCKPOT, &right_shock_msg.buf.val);
+        &kms_dbc, CAN_ID_CORNERNODE_RR_SHOCKPOT, &right_shock_msg.buf.val);
 
     daq_can.send_controller_message(left_shock_msg);
     daq_can.send_controller_message(right_shock_msg);
 
+    Serial.printf("left shock: %i\n\r", left_shock_pot.value.in);
+    Serial.printf("right shock: %i\n\r", right_shock_pot.value.in);
+
     hz_50.reset();
-  }
-
-  if (hz_20.check()) {
-    encode_can_0x390_cornernode_front_brakepressure(&kms_dbc,
-                                                    front_brake.value.in);
-    front_brake_msg.length =
-        pack_message(&kms_dbc, CAN_ID_CORNERNODE_FRONT_BRAKEPRESSURE,
-                     &front_brake_msg.buf.val);
-
-    encode_can_0x391_cornernode_rear_brakepressure(&kms_dbc,
-                                                   rear_brake.value.in);
-    rear_brake_msg.length =
-        pack_message(&kms_dbc, CAN_ID_CORNERNODE_REAR_BRAKEPRESSURE,
-                     &rear_brake_msg.buf.val);
-
-    encode_can_0x383_cornernode_steeringpot(&kms_dbc, steering_pot.value.in);
-    steering_pot_msg.length = pack_message(
-        &kms_dbc, CAN_ID_CORNERNODE_STEERINGPOT, &steering_pot_msg.buf.val);
-
-    Serial.printf("Front: %i\n\r", front_brake.value.in);
-    Serial.printf("Rear: %i\n\r", rear_brake.value.in);
-    Serial.printf("steer: %i\n\r", steering_pot.value.in);
-
-    daq_can.send_controller_message(front_brake_msg);
-    daq_can.send_controller_message(rear_brake_msg);
-    daq_can.send_controller_message(steering_pot_msg);
-
-    hz_20.reset();
   }
 }
